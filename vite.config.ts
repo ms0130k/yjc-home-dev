deaff698167baa76d530ba07f0eb0f10d39fd033
KimDonghyeon <minseok0130@gmail.com>
2025-04-24 02:27:19 +0900

    404 페이지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,23 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/', // GitHub Pages의 경우 '/repository-name/'로 설정
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        data: fileURLToPath(new URL('./yjc-data.json', import.meta.url)),
+      },
+    },
+  },
+  publicDir: 'public',
 })
